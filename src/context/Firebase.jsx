42509bae41426ef6468fc66dff88ce01ca55f561
Parth Miroliya <96381548+ParthMi@users.noreply.github.com>
2023-08-20 14:31:43 +0530

    Update Firebase.jsx

--- src/context/Firebase.jsx
+++ src/context/Firebase.jsx
@@ -8,14 +8,14 @@ import { getDatabase,set, ref, get,child } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA-OrANBt0BiaW21dhEB7NLj96iBgOaEo0",
-  authDomain: "messaging-23ec9.firebaseapp.com",
-  projectId: "messaging-23ec9",
-  storageBucket: "messaging-23ec9.appspot.com",
-  messagingSenderId: "402127668542",
-  appId: "1:402127668542:web:99a8529f7143be5add186f",
-  measurementId: "G-ME12CDDBM6",
-  databaseURL: "https://messaging-23ec9-default-rtdb.firebaseio.com/",
+  apiKey: "#",
+  authDomain: "#",
+  projectId: "#",
+  storageBucket: "#",
+  messagingSenderId: "#",
+  appId: "#",
+  measurementId: "#",
+  databaseURL: "#",
 };
 
 // Initialize Firebase
